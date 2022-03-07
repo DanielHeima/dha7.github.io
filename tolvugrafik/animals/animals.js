40a1f992a9c851e1a6f7d88af2443882d92760c6
Daníel Helgi Ágústsson <danielhelgi1@gmail.com>
2022-03-07 00:39:19 +0000

    perspective

--- tolvugrafik/animals/animals.js
+++ tolvugrafik/animals/animals.js
@@ -28,6 +28,7 @@ let panicDur = 4;
 //////////////////
 
 let MV;
+let zDist = -3.0;
 
 let sm;
 
@@ -690,10 +691,6 @@ window.onload = function init()
     let program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
     
-    //let cBuffer = gl.createBuffer();
-    //gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
-    //gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
-
     vColor = gl.getUniformLocation(program, "vColor");
     
     lineBuffer = gl.createBuffer();
@@ -705,6 +702,11 @@ window.onload = function init()
     gl.enableVertexAttribArray( vPosition );
 
     matrixLoc = gl.getUniformLocation( program, "mv" );
+    proLoc = gl.getUniformLocation( program, "projection" );
+    
+
+    let proj = perspective( 50.0, 1.0, 0.2, 100.0 );
+    gl.uniformMatrix4fv(proLoc, false, flatten(proj));
 
     sm = new SpatialManager();
     edges = new Edges();
@@ -852,9 +854,16 @@ function render()
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     
     
-    MV = mat4();
+    ///MV = mat4();
+    zDist = -2;
+    let eyesep = 0.2
+    
+    MV = lookAt( vec3(0.0-eyesep/2.0, 0.0, zDist),
+    vec3(0.0, 0.0, zDist+2.0),
+    vec3(0.0, 1.0, 0.0) );
+  
     MV = mult( MV, rotateX(-spinX) );
-    MV = mult( MV, rotateY(-spinY) ) ;
+    MV = mult( MV, rotateY(spinY) ) ;
     
     edges.render();
 
